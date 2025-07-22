9cb441dd7612cc174430f24b3d86193d41aba443
Lumin228 <6786679@gmail.com>
2025-07-22 21:58:03 +0300

    ss

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import App from './App';
+import App from './components/App/App';
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
